cf40de94353eedb440ec9b8b383b30910eb6a38c
Tumworobere <annaht.annah@gmail.com>
2022-01-21 20:21:39 +0300

    Restructure the web App

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,39 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Book from './Book';
+import { addBook, removeBook } from '../redux/books/books';
+import store from '../redux/configureStore';
+import InputBook from './inputBook';
 
 const { v4: uuidv4 } = require('uuid');
 
 const BooksList = () => {
-  const books = [
-    {
+  const [books, setBooks] = useState(store.getState().booksReducer);
+  const dispatch = useDispatch();
+
+  const submitBookToStore = ({ title, author }) => {
+    const newBook = {
       id: uuidv4(),
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-    },
-    {
-      id: uuidv4(),
-      title: 'Lord of The Rings',
-      author: 'J. R. R. Tolkien',
-    },
-    {
-      id: uuidv4(),
-      title: 'Alice in WonderLand',
-      author: 'Lewis Carroll',
-    },
-  ];
+      title,
+      author,
+    };
+
+    dispatch(addBook(newBook));
+
+    localStorage.setItem('BooksList', JSON.stringify(store.getState().booksReducer));
+    setBooks(JSON.parse(localStorage.getItem('BooksList')));
+  };
+
+  const deleteBook = (book) => {
+    dispatch(removeBook(book));
+
+    localStorage.setItem('BooksList', JSON.stringify(store.getState().booksReducer));
+    setBooks(JSON.parse(localStorage.getItem('BooksList')));
+  };
 
   return (
+    <div>
+      <ul>
+        {books.map((book) => (
+          <Book
+            key={book.id}
+            title={book.title}
+            author={book.author}
+            deleteBook={() => deleteBook(book)}
+          />
+        ))}
+      </ul>
 
-    <ul>
-      {books.map((book) => (
-        <Book
-          id={book.id}
-          key={book.id}
-          title={book.title}
-          author={book.author}
-        />
-      ))}
-    </ul>
+      <InputBook addBook={submitBookToStore} />
+    </div>
 
   );
 };
