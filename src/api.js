f6ab56ef2330a275ec69a5707222544cde1cc348
Tumworobere <annaht.annah@gmail.com>
2022-01-21 23:06:33 +0300

    Add api.js

---
+++ src/api.js
@@ -0,0 +1,39 @@
+const apiUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/qdrNrAMV3sFxA9qazfks/books';
+
+const newBook = async (book) => {
+  const result = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      item_id: book.item_id,
+      title: book.title,
+      category: book.category,
+    }),
+  });
+
+  return result.status === 201;
+};
+
+const getBooks = async () => {
+  const request = await fetch(apiUrl);
+  const books = await request.json();
+  return books;
+};
+
+const deleteBook = async (book) => {
+  const result = await fetch(`${apiUrl}/${book.item_id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      item_id: book.item_id,
+    }),
+  });
+
+  return result === 'This book was deleted!';
+};
+
+export { newBook, getBooks, deleteBook };
